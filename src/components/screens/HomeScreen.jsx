7165d2c771ae03d1b0a8734412dbb38b65268175
Manu Zarraga <manuzarraga@hotmail.com>
2023-11-23 17:27:41 -0300

    feat: added axios, paginate hook created

--- src/components/screens/HomeScreen.jsx
+++ src/components/screens/HomeScreen.jsx
@@ -1,7 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { usePokemonPaginate } from "../../hooks/usePokemonPaginate";
 
 export default function HomeScreen() {
+  const x = usePokemonPaginate();
+
   return (
     <View>
       <Text>HomeScreen</Text>
---
+++ src/hooks/usePokemonPaginate.jsx
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+export const usePokemonPaginate = () => {
+  const POKEMON_TYPE_COLORS = {
+    normal: "#A8A878",
+    fighting: "#C03028",
+    flying: "#A890F0",
+    poison: "#A040A0",
+    ground: "#E0C068",
+    rock: "#B8A038",
+    bug: "#A8B820",
+    ghost: "#705898",
+    steel: "#B8B8D0",
+    fire: "#FA6C6C",
+    water: "#6890F0",
+    grass: "#48CFB2",
+    electric: "#FFCE4B",
+    psychic: "#F85888",
+    ice: "#98D8D8",
+    dragon: "#7038F8",
+    dark: "#705860",
+    fairy: "#EE99AC",
+  };
+
+  const [pokemonList, setPokemonList] = useState([]);
+  const [nextPageUrl, setNextPageUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon?limit=10"
+  );
+
+  const loadPokemons = async () => {
+    const res = await axios.get(nextPageUrl);
+    setNextPageUrl(res.data.next);
+    mapPokemons(res.data.results);
+  };
+
+  const pokemonDetailByUrlApi = async (url) => {
+    try {
+      const res = await axios.get(url);
+      return res.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+
+  const mapPokemons = async (pokemons) => {
+    const newPokemonList = [];
+
+    for await (const pokemon of pokemons) {
+      const pokemonDetail = await pokemonDetailByUrlApi(pokemon.url);
+      newPokemonList.push({
+        id: pokemonDetail.id,
+        name: pokemonDetail.name,
+        type: pokemonDetail.types[0].type.name, // type name here
+        picture: pokemonDetail.sprites.other["official-artwork"].front_default,
+        color: POKEMON_TYPE_COLORS[pokemonDetail.types[0].type.name],
+      });
+    }
+  };
+
+  useEffect(() => {
+    loadPokemons();
+  }, []);
+
+  return "Ejecutadou";
+};
